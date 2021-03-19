a8940a6d6cb085ab3b5cb4b6625098e1f93a9fd5
HiepDo <hiepptit99@gmail.com>
2021-03-18 22:34:18 -0400

    Add regist and login feature

---
+++ README.md
@@ -0,0 +1,1 @@
+This app is for manage a retaurant
---
+++ app/src/main/java/com/hiepdh99/myorderfoodapp/LoginActivity.java
@@ -0,0 +1,84 @@
+ package com.hiepdh99.myorderfoodapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hiepdh99.myorderfoodapp.DAO.StaffDAO;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnLogin, btnRegist;
+    EditText edUsername, edPassword;
+    StaffDAO staffDAO;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+        staffDAO = new StaffDAO(this);
+        edUsername = findViewById(R.id.edUsername);
+        edPassword = findViewById(R.id.edPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegist = findViewById(R.id.btnRegist);
+        displayButton();
+        btnLogin.setOnClickListener(this);
+        btnRegist.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayButton();
+    }
+
+    private  void displayButton(){
+        boolean check = staffDAO.hasStuff();
+        if (check){
+            btnRegist.setVisibility(View.VISIBLE);
+            btnLogin.setVisibility(View.VISIBLE);
+        }
+        else{
+            btnRegist.setVisibility(View.VISIBLE);
+            btnLogin.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btnLogin:
+                login();
+                break;
+            case R.id.btnRegist:
+                regist();
+                break;
+        }
+    }
+    private void login(){
+        String username = edUsername.getText().toString();
+        String passwd = edPassword.getText().toString();
+        boolean check = staffDAO.checkStuff(username, passwd);
+        if(check){
+            Intent iHome = new Intent(this, HomeActivity.class );
+            iHome.putExtra("username", edUsername.getText().toString());
+            startActivity(iHome);
+        }else
+            Toast.makeText(this, "That bai", Toast.LENGTH_SHORT).show();
+    }
+    private void regist(){
+        Intent iRegist = new Intent(this, RegistActivity.class);
+        startActivity(iRegist);
+
+    }
+}
